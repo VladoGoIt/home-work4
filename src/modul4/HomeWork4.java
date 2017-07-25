af3ce1840498f9d4a10f26b7cfc35f59e56e5815
VladoGoIt <dzhuhalyk@gmail.com>
2017-07-26 01:45:26 +0300

    створив репозиторій і зробив перше завдання

---
+++ src/modul4/HomeWork4.java
@@ -0,0 +1,17 @@
+package modul4;
+
+public class HomeWork4 {
+
+    public static void main (String[] args){
+        num(20);
+    }
+
+    public static void num (int x){
+        int f = 0;
+            for (int i = 0; i < x; i++) {
+                f++;
+                System.out.println(f);
+            }
+        }
+
+}
