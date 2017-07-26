1c43b2098a56abc99643d4fec5c5c1cfc5325bd0
VladoGoIt <dzhuhalyk@gmail.com>
2017-07-26 04:08:19 +0300

    зробив завдання 2

--- src/modul4/HomeWork4.java
+++ src/modul4/HomeWork4.java
@@ -3,7 +3,8 @@ package modul4;
 public class HomeWork4 {
 
     public static void main (String[] args){
-        num(20);
+        num(3);
+        drawRectangle(5, 4);
     }
 
     public static void num (int x){
@@ -13,5 +14,14 @@ public class HomeWork4 {
                 System.out.println(f);
             }
         }
+    public static void drawRectangle (int x, int y){
+        char [] arr = new char [x];
+        for (int i = 0; i < x; i++) {
+            arr[i] = '+';
+        }
+        for (int i = 0; i < y; i++) {
+            System.out.println(arr);
+        }
 
+    }
 }
